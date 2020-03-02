fce52a0236d4f3f839108d46a8f8b4cbde5a0ca3
wooyee-ldq <2632230944@qq.com>
2020-03-03 00:20:44 +0800

    update2

--- Service/stu_service.py
+++ Service/stu_service.py
@@ -1,6 +1,7 @@
 # coding:utf-8
+import time
 
-
+from Model.model import Selcourses, db
 from Service.campusManage import CampusManage
 from Service.redis_service import RedisService
 
@@ -9,6 +10,7 @@ class StuService(object):
 
     @staticmethod
     def get_preview_course():
+        """获取所有可以的预选课程"""
         campus_li = CampusManage.get_all()
         for campus in campus_li:
             course_li = RedisService.get_selcourse(campus.get("id"))
@@ -18,10 +20,12 @@ class StuService(object):
 
     @staticmethod
     def get_preview_bycaid(caid):
+        """获取对应校区可以预选的课程列表"""
         return RedisService.get_selcourse(caid)
 
     @staticmethod
     def count_gpaandcredits(achi_li):
+        """计算平均绩点和已修学分"""
         crd_sum = 0
         achi_sum = 0
         gp_sum = 0
@@ -33,4 +37,69 @@ class StuService(object):
 
         return crd_sum, gp_sum/achi_sum
 
+    @staticmethod
+    def get_predone(sno):
+        """获取预选成功的课程列表"""
+        return RedisService.get_preview(sno)
+
+    @staticmethod
+    def save_preview(sno, cid_li):
+        """保存学生的预选课程"""
+        ln = len(cid_li)
+        if ln > 3:
+            return False
+        cids = ""
+        for i in range(ln):
+            cid = str(cid_li[i])
+            if i == ln-1:
+                cids = cids + cid
+                continue
+            cids = cids + cid + ","
+
+        try:
+            RedisService.preview_sel(sno, cids)
+            return True
+
+        except Exception as e:
+            print(e)
+            return False
+
+    @staticmethod
+    def save_sel_course(sid, sno, caid, cid_li):
+        """保存学生选课信息"""
+        ln = len(cid_li)
+        if ln > 3:
+            return False, list()
+        cids = list()
+        for i in range(ln):
+            try:
+                cid = str(cid_li[i])
+                num = RedisService.get_selnum(caid, cid)
+                if num == 0:
+                    continue
+                if not RedisService.selnum_sub1(caid, cid):
+                    continue
+                sel = Selcourses(sid=sid,
+                                 cid=cid,
+                                 cretime=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                db.session.add(sel)
+                cids.append(cid)
+
+            except Exception as e:
+                print(e)
+                return False, list()
+
+        try:
+            RedisService.save_sel(sno, cids)
+            RedisService.del_preview(sno)
+            db.session.commit()
+            return True, cids
+        except Exception as e:
+            print(e)
+            return False, list()
+
+    @staticmethod
+    def remove_sel_course(sid, sno, cid, caid):
+        """课程退选操作业务"""
+        return RedisService.judge_and_remove(sid, sno, cid, caid)
 
